8f0c67417c614671168ac1d3e787d2e49b9128de
Arkoprabho Bhattacharjee <bhattacharjee.arkoprabho@gmail.com>
2023-05-05 19:54:08 +0530

    Added a loading state for when the camera is being set-up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import { Camera } from "@mediapipe/camera_utils"
 import "@mediapipe/control_utils"
 import "@mediapipe/drawing_utils"
 import "@mediapipe/hands"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null)
+  const [cameraReady, setCameraReady] = useState(false)
 
   useEffect(() => {
     const camera = new Camera(videoRef.current!, {
@@ -16,7 +17,14 @@ export default function Home() {
       height: 720,
     })
 
-    camera.start()
+    camera
+      .start()
+      .then(() => {
+        setCameraReady(true)
+      })
+      .catch((error) => {
+        console.error("Failed to start camera:", error)
+      })
 
     return () => {
       camera.stop()
@@ -25,10 +33,16 @@ export default function Home() {
 
   return (
     <>
+      {!cameraReady && (
+        <div className="absolute inset-0 flex items-center justify-center">
+          <p className="text-black text-xl font-semibold">Setting up the camera ...</p>
+        </div>
+      )}
       <div className="relative">
         <video
           ref={videoRef}
-          className="w-full h-full object-cover transform -scale-x-100"
+          className={`w-full h-full object-cover transform -scale-x-100 ${cameraReady ? "" : "hidden"
+            }`}
           autoPlay
           playsInline
         />
