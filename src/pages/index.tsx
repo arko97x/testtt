989d6507f666eb28ceb83635d2d5e4439489c79c
Arkoprabho Bhattacharjee <bhattacharjee.arkoprabho@gmail.com>
2023-05-05 20:07:43 +0530

    Added the react-loading package for realistic loading animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import "@mediapipe/control_utils"
 import "@mediapipe/drawing_utils"
 import "@mediapipe/hands"
 import { useEffect, useRef, useState } from "react"
+import ReactLoading from "react-loading"
 
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null)
@@ -33,15 +34,18 @@ export default function Home() {
 
   return (
     <>
-      {!cameraReady && (
+      {cameraReady && (
         <div className="absolute inset-0 flex items-center justify-center">
-          <p className="text-black text-xl font-semibold">Setting up the camera ...</p>
+          <div className="flex items-end">
+            <p className="text-black text-xl font-semibold">Setting up the camera</p>
+            <ReactLoading type="bubbles" color="black" height={22} width={24} />
+          </div>
         </div>
       )}
       <div className="relative">
         <video
           ref={videoRef}
-          className={`w-full h-full object-cover transform -scale-x-100 ${cameraReady ? "" : "hidden"
+          className={`w-full h-full object-cover transform -scale-x-100 ${!cameraReady ? "" : "hidden"
             }`}
           autoPlay
           playsInline
