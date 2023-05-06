7bbe4cf63f1a4e17c55cf480a985635b7b5153e1
Arkoprabho Bhattacharjee <bhattacharjee.arkoprabho@gmail.com>
2023-05-06 20:45:18 +0530

    Brought back the camera set-up message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Camera } from "@mediapipe/camera_utils"
 import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils"
 import { HAND_CONNECTIONS, Hands } from "@mediapipe/hands"
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
+import ReactLoading from "react-loading"
 
 export default function Home() {
   const videoRef = useRef<HTMLVideoElement>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [cameraReady, setCameraReady] = useState(false)
 
   useEffect(() => {
     const videoElement = videoRef.current
@@ -69,7 +71,14 @@ export default function Home() {
       width: window.innerWidth,
       height: window.innerHeight,
     })
-    camera.start()
+    camera
+      .start()
+      .then(() => {
+        setCameraReady(true)
+      })
+      .catch((error) => {
+        console.error("Failed to start camera:", error)
+      })
 
     return () => {
       camera.stop()
@@ -79,7 +88,15 @@ export default function Home() {
 
   return (
     <>
-      <div className="w-screen h-screen">
+      <div className="w-screen h-screen bg-white">
+        {!cameraReady && (
+          <div className="w-full h-full flex items-center justify-center">
+            <div className="flex items-end">
+              <p className="text-black text-lg font-medium">Setting up the camera</p>
+              <ReactLoading type="bubbles" color="black" height={22} width={24} />
+            </div>
+          </div>
+        )}
         <div className="w-full flex items-start justify-center pt-9 md:pt-10">
           <video
             ref={videoRef}
@@ -89,7 +106,7 @@ export default function Home() {
           />
           <canvas
             ref={canvasRef}
-            className={`output_canvas w-[80vw] md:w-[60vw] lg:w-[50vw] xl:w-[50vw] h-auto object-cover transform -scale-x-100 rounded-3xl border-2 border-transparent p-0.5`}
+            className={`output_canvas w-[80vw] md:w-[60vw] lg:w-[50vw] xl:w-[50vw] h-auto object-cover transform -scale-x-100 rounded-3xl border-2 border-transparent p-0.5 ${cameraReady ? "" : "hidden"}`}
             width={1280}
             height={720}
           />
